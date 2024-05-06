d57360f31261108893142ae46c7b00f5f510c59d
SudoAldebaran <rayanmagniac@gmail.com>
2024-05-06 15:38:47 +0200

    Init

---
+++ WpfApp1/evalTables.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logique d'interaction pour evalTables.xaml
+    /// </summary>
+    public partial class evalTables : Window
+    {
+        private Random rd;
+        private int NumQuestion = 0;
+        private int NumEssai = 0;
+        private int NbPoints = 0;
+        private int Resultat = 0;
+        private int table;
+        public evalTables()
+        {
+            InitializeComponent();
+            rd = new Random();
+            string txt;
+            txt = "  Après avoir choisi la table, vous aurez 5 questions; pour chaque question vous aurez deux essais.\n Si vous répondez bien au premier essai vous aurez 2 points mais ";
+            txt += "1 point si vous répondez seulement \n au deuxième essai et 0 point sinon. Vous serez ainsi noté sur 10. ";
+            this.tBbkPresentation.Text = txt;
+            // code à compléter pour charger la listBox
+            for (int i = 1; i <= 10; i++)
+            {
+                this.lstTable.Items.Add(i);
+            }
+        }
+
+        private void InitQuestion()
+        {
+            this.NumQuestion++;
+            if (this.NumQuestion <= 5)
+            {
+                int  nbAlea;
+                this.NumEssai = 1;
+                this.txtReponse.Focus();
+                //code pour faire apparaitre la question et calculer le bon résultat
+                
+            }
+            else
+            {
+                MessageBox.Show("C'est terminé !!");
+                this.txtReponse.IsEnabled = false;
+            }
+        }
+        private void btnCommencer_Click(object sender, RoutedEventArgs e)
+        {
+            this.btnCommencer.IsEnabled = false;
+            table = Convert.ToInt32(lstTable.SelectedValue);
+            this.lstTable.IsEnabled = false;
+            InitQuestion();
+        }
+
+        private void txtReponse_KeyDown(object sender, KeyEventArgs e)
+        {
+           try
+            {
+               if (e.Key == Key.Enter)
+                {
+                    // code pour tester la saisie et gérer les points selon la réponse et l'essai
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Saisir un nombre svp");
+                this.txtReponse.Text = "";
+            }
+        }
+
+
+    }
+}
---
+++ WpfApp1/voirTables.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logique d'interaction pour voirTables.xaml
+    /// </summary>
+    public partial class voirTables : Window
+    {
+        public voirTables()
+        {
+            InitializeComponent();
+            // Code à compléter
+            
+        }
+
+        private void lstTables_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            this.txtTable.Text = "";
+            //string table = (string)this.lstTables.SelectedItem.ToString();
+
+            string table = (lstTables.SelectedItem as ListBoxItem)?.Content?.ToString();
+
+            int tableS = Convert.ToInt32(table);
+
+
+
+            for(int i = 1; i <= 10; i++)
+            {
+                this.txtTable.Text += $"{table} × {i} = {tableS * i}\r\n";
+            }
+
+
+
+        }
+
+    }
+}
